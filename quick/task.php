2ea279e003abc9c4b7476e8b43f96ff60fb46d49
zhangfq <zhangfq@56cy.com>
2018-12-12 11:43:44 +0800

    test

--- quick/task.php
+++ quick/task.php
@@ -11,6 +11,10 @@ $serv = new swoole_server('0.0.0.0', 9501);
 
 $serv->set(array('task_worker_num' => 4));
 
+$serv->on('connect', function ($serv, $fd){
+    echo "链接上了: ".PHP_EOL;
+});
+
 $serv->on('receive', function ($serv, $fd, $from_id, $data){
     $task_id = $serv->task($data);
     echo "异步 ID: " . $task_id.PHP_EOL;
